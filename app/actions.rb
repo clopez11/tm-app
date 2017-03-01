8f2ab47fda8d377c3a5fa6da00680682a47bd511
clopez11 <char.lopez11@gmail.com>
2017-02-28 23:09:53 -0500

    adding erb files

--- app/actions.rb
+++ app/actions.rb
@@ -9,5 +9,28 @@ end
 
 # Messages
 get '/messages' do
+  @messages = Message.all
   erb :'messages/index'
+end
+
+# Messages New
+get '/messages/new' do
+  erb :'messages/new'
+end
+
+# Message Posts
+post '/messages' do
+  @message = Message.new(
+    title: params[:title],
+    content: params[:content],
+    author:  params[:author]
+  )
+  @message.save
+  redirect '/messages'
+end
+
+# Message Detail
+get '/messages/:id' do
+  @message = Message.find params[:id]
+  erb :'messages/show'
 end
